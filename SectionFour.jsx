ea090ad8439d42aa7998aa63199ef12657507b4c
Mohammad-Naved <74970362+Mohammad-Naved@users.noreply.github.com>
2021-02-01 21:53:22 +0530

    Update SectionFour.jsx

--- SectionFour.jsx
+++ SectionFour.jsx
@@ -7,14 +7,14 @@ const SectionFour = () => {
     <div class="row">
       <div class="col-lg-6 col-md-6 col-sm-6">
         <h1 class="big-heading">Stuck in life or stuck in your career ???</h1>
-        <img class="img-1 mx-auto d-block" src="Assets/nikhita-s-NsPDiPFTp4c-unsplash.jpg" alt="" srcset="" />
+        <img class="img-1 mx-auto d-block" src="nikhita-s-NsPDiPFTp4c-unsplash.jpg" alt="" srcset="" />
         <h1 class="big-heading">
           We help you overcome the mental block in your life
         </h1>
       </div>
 
       <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-2 mx-auto d-block" src="/Assets/church-of-the-king-j9jZSqfH5YI-unsplash.jpg" alt=""
+        <img class="img-2 mx-auto d-block" src="church-of-the-king-j9jZSqfH5YI-unsplash.jpg" alt=""
           srcset="" />
         <h1 class="big-heading mx-auto">
           Join our Initiative called as HealMyMind
