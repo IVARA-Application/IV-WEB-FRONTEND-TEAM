2babb07d60b06e0c663c650c9b922bc6f6c9441d
Dhananjay Sharma <sdhananjay006@gmail.com>
2021-02-01 23:05:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./Components/Home";
-
-
+import Middle from "./Components/Middle.jsx";
+import {Form,Footer,Copyright} from "./Components/Contact.js";
 import NotFound from "./Components/NotFound";
 
 
@@ -18,8 +18,12 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home />
+          <Middle />
+          <Form />
+          <Footer />
+          <Copyright/>
         </Route>
-
+        
         
         <Route path="*">
           <NotFound />
@@ -27,6 +31,7 @@ function App() {
       </Switch>
     </BrowserRouter>
     
+
   );
 }
 
