0841050533708790e343186f79f0661fe9b5d52d
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-02-07 19:55:53 +0530

    Form updated for googleOauth

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -21,54 +21,54 @@ const Login = () => {
    const [number, setnumber] = useState("");
 
 
-   const responseGoogle = async (authResult) => {
-    try {
-      if (authResult['code']) {
-        const result = await login(authResult['code']);
-        console.log(authResult);
-        // props.login(result);
-      } else {
-        throw new Error(authResult);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  //  const onGoogle = () =>{
-  //   axios
-  //   .get(baseUrl + "users/auth/google", {headers: {"Access-Control-Allow-Origin": true}})
-    
-  //   .then(
-  //     (response) => {
-  //       if (response.ok) {
-  //         return response;
-  //       } else {
-  //         var error = new Error(
-  //           "Error " + response.status + ": " + response.statusText
-  //         );
-  //         error.response = response;
-  //         throw error;
-  //       }
-  //     },
-  //     (error) => {
-  //       throw error;
-  //     }
-  //   )
-  //   .then((response) => response.json())
-  //   .then((response) => {
-  //     if (response.success) {
-  //       localStorage.setItem("token", response.token);
-  //       setUser(response.user);
-  //       setAuth(true);
-  //       setToken(response.token);
+  //  const responseGoogle = async (authResult) => {
+  //   try {
+  //     if (authResult['code']) {
+  //       const result = await login(authResult['code']);
+  //       console.log(authResult);
+  //       // props.login(result);
   //     } else {
-  //       var error = new Error("Error " + response.status);
-  //       error.response = response;
-  //       throw error;
+  //       throw new Error(authResult);
   //     }
-  //   })
-  //  }
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+   const onGoogle = () =>{
+    axios
+    .get(baseUrl + "users/auth/google")
+    
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        throw error;
+      }
+    )
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.success) {
+        localStorage.setItem("token", response.token);
+        setUser(response.user);
+        setAuth(true);
+        setToken(response.token);
+      } else {
+        var error = new Error("Error " + response.status);
+        error.response = response;
+        throw error;
+      }
+    })
+   }
 
     const onSubmits=(event)=>{
         event.preventDefault();
@@ -82,7 +82,7 @@ const Login = () => {
   return (
     <> 
       <div className="container-fluid login-container">
-        <form onSubmit={onSubmits}>
+        <form action="http://localhost:5000/users/auth/google">
           <div className="container login ">
             <h1 className="loginHeading">Login</h1>
             <h4 className="subHeading">Or Create a new account</h4>
@@ -99,17 +99,17 @@ const Login = () => {
                 aria-describedby="addon-wrapping"
               />
             </div>
-            <button className=" submitButton" type="submit">Login</button>
-            {/* <button className="emailId  submitButton" >Continue with Google</button> */}
-            <GoogleLogin
+            {/* <button className=" submitButton" type="submit">Login</button> */}
+            <button type="submit" className="emailId  submitButton" >Continue with Google</button>
+            {/* <GoogleLogin
              clientId="604975047284-im9me9431a4m8pae0e8qmmn5a89dc1rs.apps.googleusercontent.com"
              buttonText="Login with google"
              responseType="code" 
              redirectUri="postmessage"
              onSuccess={responseGoogle}
              onFailure={responseGoogle}
-             cookiePolicy={'single_host_origin'}
-            />
+             cookiePolicy={'single_host_origin'} */}
+            {/* /> */}
           </div>
         </form>
         <p className="contact">Having trouble? Please contact help@iventorsinitiatives.com for further support.</p>
--- src/api/googleAuth.js
+++ src/api/googleAuth.js
@@ -1,21 +1,21 @@
-import {baseUrl} from '../constants'
+import { baseUrl } from '../constants'
 import axios from 'axios'
 const login = async (code) => {
-    const body = {"code" : code};
-    return axios
-     .post(baseUrl + "api/auth/google",
-     {headers:{"Access-Control-Allow-Origin":true}},
-     {
-        code: `${code}`,
-      })
+  const body = { "code": code };
+  return axios
+    .post(baseUrl + "api/auth/google", JSON.stringify(code))
+    //  {headers:{"Access-Control-Allow-Origin":true}},
+    //  {
+    //     code: `${code}`,
+    //   })
     .then((res) => {
-        console.log(res);
+      console.log(res);
       if (res.ok) {
         return res.json();
       } else {
         return Promise.reject(res);
       }
     });
-  };
-  
-  export { login };
+};
+
+export { login };
